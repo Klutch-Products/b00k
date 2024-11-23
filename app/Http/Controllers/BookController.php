5fa99822f0620614c4c70873593087eea5b798a1
AJ Javadi <amirjavadi25@gmail.com>
2024-11-22 22:20:47 -0600

    updated book controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,6 +6,7 @@ use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use App\Http\Controllers\AuthorController;
 
 class BookController extends BaseController
 {
